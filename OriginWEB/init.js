061bc17402dfb3ef40e614bc4dc6f45773dce19d
unblessed <ykkrrpro123@gmail.com>
2026-09-14 23:07:25 +0300

    Update init.js

--- OriginWEB/init.js
+++ OriginWEB/init.js
@@ -185,7 +185,6 @@ frame.classList.add(`${deviceType}Mode`);
 // load clock app
 await loadHTMLInto("#app_clock .appDisplay", "/OriginWEB/appData/app_clock/html/html.html");
 await loadHTMLInto("#app_calculator .appDisplay", "/OriginWEB/appData/app_calculator/html/html.html");
-await loadHTMLInto("#app_settings .appDisplay", "/OriginWEB/appData/app_settings/html/html.html");
 
 await loadAppLayout();
 await cleanupEmptyScreens();
